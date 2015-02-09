1ee4cb8cfb1f0eeb7e9a4b2b77c9c7f0fd1e619a
dimkl <dimitris.klouvas@gmail.com>
2015-02-09 21:26:35 +0200

    updated chrome extension version

--- chrome/AFBShare/content.js
+++ chrome/AFBShare/content.js
@@ -1,9 +1,54 @@
+var yUrlFormat='https://www.youtube.com/watch?v=';
+var cases={
+	'html5':function(){
+		var yvideo=document.querySelector('[data-videoid]');
+		return yvideo? yvideo.getAttribute('data-videoid'):null;
+	}
+	,'embedded':function(){
+		var format='https://www.youtube.com/embed';
+		var regex=/https\:\/\/www\.youtube\.com\/embed\/(.*)\?.*/gi;
+
+		var yvideo=document.querySelector('iframe[src^="'+format+'"]');
+
+		return yvideo?yvideo.src.replace(regex,'$1'):null;
+	}
+	,'full':function(){
+		var format='https://www.youtube.com/watch?v=';
+		var regex=/https\:\/\/www\.youtube\.com\/watch?.*v\=(.*)\&.*/gi;
+
+		var yvideo=document.querySelector('iframe[src^="'+format+'"]');
+
+		return yvideo?yvideo.src.replace(regex,'$1'):null;
+	}
+	, 'shared':function(){
+		var format='http://youtu.be/';
+		var regex=/https\:\/\/youtu\.be\/(.*)/gi;
+
+		var yvideo=document.querySelector('iframe[src^="'+format+'"]');
+
+		return yvideo?yvideo.src.replace(regex,'$1'):null;
+	}
+	,'emebed-object':function(){
+		var format='http://www.youtube.com/v/';
+		var regex=/https\:\/\/www\.youtube\.com\/v\/(.*)/gi;
+
+		var yvideo=document.querySelector('[src^="'+format+'"]');
+
+		return yvideo?yvideo.src.replace(regex,'$1'):null;
+	}
+};
+
 chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
 	switch(message.type) {
 		case "open-video":
-		var yvideo=document.querySelector('[data-videoid]');
-		var yurlFormat='https://www.youtube.com/watch?v=';
-		yvideo? window.open(yurlFormat+yvideo.getAttribute('data-videoid')):'';
+		var yvideoid;
+		for(var i in cases){
+			yvideoid=cases[i]();
+
+			if(yvideoid)
+				break;
+		}
+		yvideoid? window.open(yUrlFormat+yvideoid):'';
 		break;
 	}
 });
