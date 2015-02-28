2574b875f6889493fdd256d6528740be99b701af
dimkl <dimitris.klouvas@gmail.com>
2015-03-01 00:12:10 +0200

    fixed endless loop bug updated version to 2.1

--- firefox/AFBShare/data/afbshare-blocker.js
+++ firefox/AFBShare/data/afbshare-blocker.js
@@ -1,31 +1,36 @@
 var MAX_INTERVAL_COUNTER = 50;
 var popupIdsDictionary = {
-    "www.latesthackingnews.com": "#wp_social_popup_and_get_traffic",
-    "www.enhmerwsh.gr": "#wp_social_popup_and_get_traffic",
-    "tonwtiko.com": "#fblikepop,#fblikebg"
+	"www.latesthackingnews.com": "#wp_social_popup_and_get_traffic",
+	"www.enhmerwsh.gr": "#wp_social_popup_and_get_traffic",
+	"tonwtiko.com": "#fblikepop,#fblikebg",
+	"kafeneio-gr.blogspot.gr":"#fanback",
+	"periergaa.blogspot.gr":"#fanback"
 };
 
 function blockShare() {
-    var popup;
-    if (popupIdsDictionary[location.host]) {
-        popup = document.querySelectorAll(popupIdsDictionary[location.host]);
+	var popup=null;
+    var removed=false;
+
+	if (popupIdsDictionary[location.host]) {
+		popup = document.querySelectorAll(popupIdsDictionary[location.host]);
         //dont look in dictionary
         popupIdsDictionary = [];
     }
     for (var domain in popupIdsDictionary) {
-        popup = document.querySelectorAll(popupIdsDictionary[domain])
-        if (!popup) break;
+    	popup = document.querySelectorAll(popupIdsDictionary[domain])
+    	if (!popup) break;
     }
+
     if (popup && popup.length) {
-        for (var i = 0; i < popup.length; i++) popup[i] && popup[i].remove();
+    	for (var i = 0; i < popup.length; i++) removed = removed || (popup[i] && typeof popup[i].remove=="function" &&  popup[i].remove());
     } else if (popup) {
-        popup.remove();
-    } else {
-        return false;
-    }
-    return true;
+    	removed=removed || (typeof popup.remove=="function" && popup.remove());
+    } 
+
+    return removed;
 }
 var interval_counter = 0;
 var interval = window.setInterval(function() {
-    (blockShare() || interval_counter >= MAX_INTERVAL_COUNTER) && window.clearInterval(interval);
+	interval_counter++;
+	(blockShare() || (interval_counter >= MAX_INTERVAL_COUNTER)) && window.clearInterval(interval);
 }, 200);
